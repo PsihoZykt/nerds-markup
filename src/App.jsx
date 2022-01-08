3097522e45670607c70fd67b8cd12fabab5cd9bf
psihozykt <psihozykt@gmail.com>
2022-01-08 19:28:13 +0900

    initial release

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './app.module.css'
+const App = () => {
+    return (
+        <div className={s.wrapper}>
+          <div className={s.topMenu}>
+
+          </div>
+          <div className={s.mainPage}>
+
+          </div>
+          <div className={s.footer}>
+
+          </div>
+        </div>
+    );
+};
+
+export default App;
